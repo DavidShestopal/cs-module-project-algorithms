624516568a8b94b1e219d62b4b249fd9541c40ae
David Shestopal <dshestopal@yahoo.com>
2020-07-22 13:23:24 -0700

    some of them solved

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,12 +2,19 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    altered = [number for number in arr if number != 0]
+    zero = [number for number in arr if number is 0]
+    altered.extend(zero)
+    return altered
 
 
+print(moving_zeroes([0, 3, 1, 0, -2]))
+print(moving_zeroes([4, 2, 1, 5]))
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
